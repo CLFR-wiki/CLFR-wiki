784790b3b1df2f7bc2ec11709e45a6ba35f590ba
oxy <116314324+oxy-corp@users.noreply.github.com>
2025-03-30 13:17:15 +0100

    Add dynamic price files

---
+++ src/data/blankPrices.js
@@ -0,0 +1,52 @@
+export const pricing = {
+  // teamName: {
+  //  subTeamName: { tempBuy: "0B$", permBuy: "0B$" },
+  //},
+  //
+  // If there is no temp buy its "N/A", if it needs a gamepass its "Requires Gamepass"
+  //
+  
+  centumSecurity: {
+    seasonedSecurity: { tempBuy: "4B$", permBuy: "12B$" },
+    specialTacticsUnit: { tempBuy: "6B$", permBuy: "14B$" },
+    detachment26: { tempBuy: "Requires Gamepass", permBuy: "Requires Gamepass" },
+    securityBranchCommander: { tempBuy: "16B$", permBuy: "Requires Gamepass" },
+  },
+  bioContainmentUnit: {
+    veteranHandler: { tempBuy: "4B$", permBuy: "12B$" },
+    daredevilHandlerSquad: { tempBuy: "6B$", permBuy: "14B$" },
+    anomalousContainmentUnit: { tempBuy: "8B$", permBuy: "16B$" },
+    biohazardControlDirector: { tempBuy: "N/A", permBuy: "16B$" },
+  },
+  facilityResponseUnit: {
+    seasonedInterventionTeam: { tempBuy: "4B$", permBuy: "12B$" },
+    rapidControlSquad: { tempBuy: "6B$", permBuy: "14B$" },
+    crisisWiredUnit: { tempBuy: "8B$", permBuy: "Requires Gamepass" },
+    teamOversight: { tempBuy: "16B$", permBuy: "Requires Gamepass" },
+  },
+  administrativeCommittee: {
+    departmentOfHumanResources: { tempBuy: "4B$", permBuy: "12B$" },
+    chairman: { tempBuy: "4B$", permBuy: "Requires Gamepass" },
+  },
+  theOsirisCollective: {
+    seasonedOsirisCollective: { tempBuy: "4B$", permBuy: "12B$" },
+    osirisStrikeTeam: { tempBuy: "6B$", permBuy: "14B$" },
+    osirisCleric: { tempBuy: "Requires Gamepass", permBuy: "Requires Gamepass" },
+    osirisProphet: { tempBuy: "16B$", permBuy: "Requires Gamepass" },
+  },
+  facilityOversight: {
+    facilityOversight: { tempBuy: "20B$", permBuy: "Requires Gamepass" },
+  },
+  researchSubjects: {
+    seasonedSubject: { tempBuy: "4B$", permBuy: "12B$" },
+    highRiskSubject: { tempBuy: "Requires Gamepass", permBuy: "Requires Gamepass" },
+    housingOverwatch: { tempBuy: "8B$", permBuy: "16B$" },
+    godfather: { tempBuy: "8B$", permBuy: "16B$" },
+  },
+  healthAndResearch: {
+    virologistStaff: { tempBuy: "4B$", permBuy: "12B$" },
+    experimentalUnit: { tempBuy: "6B$", permBuy: "14B$" },
+    healthAndResearchDirector: { tempBuy: "16B$", permBuy: "Requires Gamepass" },
+  },
+};
+  
---
+++ src/data/gamepassPrice.js
@@ -0,0 +1,63 @@
+export const gamepasses = {
+    unlimitedClearance: {
+      description:
+        "Gain Access to the entire facility! from the control room, to every restricted area possible. The possibilities are limitless. Does not grant access to spawns",
+      originalPrice: "310",
+      discount: "0.2",
+    },
+    selfDefensePack: {
+      description:
+        "Be Prepared for anything!<br><br> - Glock-17<br> - Gives 10 Armor Plating<br> - Spawns In Vendor",
+      originalPrice: "110",
+      discount: "0",
+    },
+    hostileForcesPack: {
+      description:
+        "Raid centum laboratories, steal important files. Uncover the truth.<br><br> - Early Unlocks Osiris Collective<br> - Early Unlocks Seasoned Collective & Seasoned Subject<br> - Unlocks Osiris Strike Team, Godfather & Housing Overwatch<br> - Unlocks Osiris Clerics<br> - Unlocks High-Risk Subjects<br> - Bypass Osiris Collective Limit (Does <b>Not</b> Bypass Clerics)",
+      originalPrice: "550",
+      discount: "0",
+    },
+    enterprisePack: {
+      description:
+        "The Combination of all the Civil Offices of Centum Labs.<br><br> - Unlocks Experimental Unit<br> - Unlocks Medical Staff<br> - Unlocks Virologist Staff",
+      originalPrice: "310",
+      discount: "0",
+    },
+    executivesPack: {
+      description:
+        "This pack will help provide you with the ability to help lead your choice of department!<br><br> - Unlocks Security Branch Commander<br> - Unlocks Biohazard Control Director<br> - Unlocks CIT Oversight<br> - Unlocks Osiris Prophet<br> - Unlocks Director Of Biology",
+      originalPrice: "1655",
+      discount: "0",
+    },
+    facilityOversightPack: {
+      description:
+        "Control Centum Laboratories with Facility Oversight!<br><br> - Unlock Facility Oversight<br> - Desert Eagle!<br> - SIGMCX<br> - Security Guarding Perms!<br> - Facility Announcements!<br> - Total Facility Clearance<br> - Limit Bypass",
+      originalPrice: "2155",
+      discount: "0",
+    },
+    assaultRiflePack: {
+      description:
+        "\"Gamepass give gun gun shoot kill\" - coolup5<br><br> - Unlock AK-100 Rifle!<br> - 3/3 Stock<br> - Spawns In Vendor<br> - Not on Health & Research Team",
+      originalPrice: "575",
+      discount: "0",
+    },
+    specialSecurityForcesPack: {
+      description:
+        "\"Gamepass give gun gun shoot kill\" - coolup5<br><br> - Unlock AK-100 Rifle!<br> - 3/3 Stock<br> - Spawns In Vendor<br> - Not on Health & Research Team",
+      originalPrice: "575",
+      discount: "0",
+    },
+    containmentForcesPack: {
+      description:
+        "\"You mean like the BCU one or the old old gamepass\" - jasiuge<br><br> - Grants Access to Biological Containment Unit.<br> - Grants Access to Veteran Handlers. & SIG MPX<br> - Grants Access to Daredevil Handlers.<br> - Grants Access to Anomalous Containment Unit<br> - Bypass Biohazard Team Limit! (Up to 2 Slots)",
+      originalPrice: "510",
+      discount: "0",
+    },
+    facilityResponsePack: {
+      description:
+        "Fighters in the Shadows<br><br> - Grants Access to Facility Response Unit<br> - Grants Access to Seasoned Intervention Team.<br> - Grants Access to Rapid Control Squad.<br> - Grants Access to Wired Unit (No Limit Bypass)<br> - Bypass Facility Response Team Limit! (Up to 2 Slots)",
+      originalPrice: "655",
+      discount: "0",
+    },
+  };
+  
