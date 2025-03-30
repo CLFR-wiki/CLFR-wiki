ffaa55c0e96af4363a48208650f6f7e5924ef309
oxy <116314324+oxy-corp@users.noreply.github.com>
2025-03-30 16:00:04 +0100

    Add viruses to compliment page

---
+++ src/data/viruses.js
@@ -0,0 +1,14 @@
+export const viruses = {
+    "BIIR-10": {
+        slug: "biir-10",
+        logo: "/viruses/BIIR-10.png",
+        slangName: "Iron Man",
+        scientificName: "BIIR-10",
+        expandedScientificName: "Bio-Entry Iron Rust Bacteria",
+        description: "",
+        location: "2B Biostorage 1",
+        effects: ["High Health", "High Damage Resistence", "Attacks two-hit every non-infected player and stuns"],
+        weaknesses: ["Fire"],
+        curable: true
+    },
+}
