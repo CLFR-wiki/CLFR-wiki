f4274edbb978530425a80ff205c36023a70d79b9
vexalous <vexalous@hotmail.com>
2025-04-02 22:01:40 -0400

    Update gamepassPrice.js

--- src/data/gamepassPrice.js
+++ src/data/gamepassPrice.js
@@ -4,60 +4,112 @@ export const gamepasses = {
         "Gain Access to the entire facility! from the control room, to every restricted area possible. The possibilities are limitless. Does not grant access to spawns",
       originalPrice: "310",
       discount: "0.2",
+      isOnSale: true,
     },
     selfDefensePack: {
       description:
         "Be Prepared for anything!<br><br> - Glock-17<br> - Gives 10 Armor Plating<br> - Spawns In Vendor",
       originalPrice: "110",
       discount: "0",
+      isOnSale: true,
     },
     hostileForcesPack: {
       description:
         "Raid centum laboratories, steal important files. Uncover the truth.<br><br> - Early Unlocks Osiris Collective<br> - Early Unlocks Seasoned Collective & Seasoned Subject<br> - Unlocks Osiris Strike Team, Godfather & Housing Overwatch<br> - Unlocks Osiris Clerics<br> - Unlocks High-Risk Subjects<br> - Bypass Osiris Collective Limit (Does <b>Not</b> Bypass Clerics)",
       originalPrice: "550",
       discount: "0",
+      isOnSale: true,
     },
     enterprisePack: {
       description:
         "The Combination of all the Civil Offices of Centum Labs.<br><br> - Unlocks Experimental Unit<br> - Unlocks Medical Staff<br> - Unlocks Virologist Staff",
       originalPrice: "310",
       discount: "0",
+      isOnSale: true,
     },
     executivesPack: {
       description:
         "This pack will help provide you with the ability to help lead your choice of department!<br><br> - Unlocks Security Branch Commander<br> - Unlocks Biohazard Control Director<br> - Unlocks CIT Oversight<br> - Unlocks Osiris Prophet<br> - Unlocks Director Of Biology",
       originalPrice: "1655",
       discount: "0",
+      isOnSale: true,
     },
     facilityOversightPack: {
       description:
         "Control Centum Laboratories with Facility Oversight!<br><br> - Unlock Facility Oversight<br> - Desert Eagle!<br> - SIGMCX<br> - Security Guarding Perms!<br> - Facility Announcements!<br> - Total Facility Clearance<br> - Limit Bypass",
       originalPrice: "2155",
       discount: "0",
+      isOnSale: true,
     },
     assaultRiflePack: {
       description:
         "\"Gamepass give gun gun shoot kill\" - coolup5<br><br> - Unlock AK-100 Rifle!<br> - 3/3 Stock<br> - Spawns In Vendor<br> - Not on Health & Research Team",
       originalPrice: "575",
       discount: "0",
+      isOnSale: true,
     },
     specialSecurityForcesPack: {
       description:
         "\"Gamepass give gun gun shoot kill\" - coolup5<br><br> - Unlock AK-100 Rifle!<br> - 3/3 Stock<br> - Spawns In Vendor<br> - Not on Health & Research Team",
       originalPrice: "575",
       discount: "0",
+      isOnSale: true,
     },
     containmentForcesPack: {
       description:
         "\"You mean like the BCU one or the old old gamepass\" - jasiuge<br><br> - Grants Access to Biological Containment Unit.<br> - Grants Access to Veteran Handlers. & SIG MPX<br> - Grants Access to Daredevil Handlers.<br> - Grants Access to Anomalous Containment Unit<br> - Bypass Biohazard Team Limit! (Up to 2 Slots)",
       originalPrice: "510",
       discount: "0",
+      isOnSale: true,
     },
     facilityResponsePack: {
       description:
         "Fighters in the Shadows<br><br> - Grants Access to Facility Response Unit<br> - Grants Access to Seasoned Intervention Team.<br> - Grants Access to Rapid Control Squad.<br> - Grants Access to Wired Unit (No Limit Bypass)<br> - Bypass Facility Response Team Limit! (Up to 2 Slots)",
       originalPrice: "655",
       discount: "0",
+      isOnSale: true,
     },
+    ramboPack: {
+      description:
+        "With all the might and power under ones grasp, what is there that can be done for a man, what does he have to offer when he has nothing else to lose? short answer, Destruction.<br><br>- Permanent M249 SAW<br>- 25+ Armor Pack.<br><br>- 2/2 Stock<br>- Spawns In Vendor<br>- Not on Health & Research Team",
+      originalPrice: "740",
+      newPrice: "899",
+      discount: "0",
+      isOnSale: false,
+    },
+    deltaForceJuggernaut: {
+      description:
+        "Get an exclusive Cosmetic Jugg inside the Hostile Lockers, comes with Night-vision Quad Goggles.",
+      originalPrice: "350",
+      discount: "0",
+      isOnSale: false,
+    },
+    christmasHolidayPack: {
+        description:
+          "- Exclusive BCU Christmas Morph.<br>- Christmas Research Subject Sweaters.",
+        originalPrice: "200",
+        discount: "0",
+        isOnSale: false,
+      },
+    factionsExtensionPack: {
+        description:
+          "The factions extensions pack adds the ability to make you stand out among your fellow members of the facility, and also gives you a greater ability to have as much fun as possible<br><br>- 1 Daily Free Gang Riot/Raid.<br>- 2x Boost During Faction Events.<br>- Ability to add Decal Image to Faction.<br>- Free Name Change Perms.<br>- Create Faction for Free.",
+        originalPrice: "699",
+        discount: "0",
+        isOnSale: false,
+      },
+    theWickedCollective: {
+        description:
+          "Cosmetic Morphs for<br>- Osiris Collective.<br>- Seasoned Osiris.<br>- Osiris Strike Team.<br>- Osiris Clerics.<br>- Osiris Prophet.",
+        originalPrice: "300",
+        discount: "0",
+        isOnSale: false,
+      },
+    jackOSecurity: {
+        description:
+          "Unlock the Jack-O Security Morphs perm for.<br><br>- Centum Security<br>- Seasoned Centum Security<br>- Special Tactics Unit<br>- Detachment 26<br>- Security Branch Commander",
+        originalPrice: "225",
+        discount: "0",
+        isOnSale: false,
+      },
   };
-  
