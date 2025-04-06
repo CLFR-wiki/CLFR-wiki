82290b31114abd15269d9e6a8fdbf622fd02bf67
vexalous <vexalous@hotmail.com>
2025-04-06 14:50:23 -0400

    Update viruses.js

--- src/data/viruses.js
+++ src/data/viruses.js
@@ -1,14 +1,24 @@
 export const viruses = {
-    "BIIR-10": {
-        slug: "biir-10",
-        logo: "/viruses/BIIR-10.png",
-        slangName: "Iron Man",
-        scientificName: "BIIR-10",
-        expandedScientificName: "Bio-Entry Iron Rust Bacteria",
-        description: "",
-        location: "2B Biostorage 1",
-        effects: ["High Health", "High Damage Resistence", "Attacks two-hit every non-infected player and stuns"],
-        weaknesses: ["Fire"],
-        curable: true
-    },
+"BIIR-10": {
+slug: "biir-10",
+logo: "/viruses/BIIR-10.png",
+slangName: "Iron Man",
+scientificName: "BIIR-10",
+expandedScientificName: "Bio-Entry Iron Rust Bacteria",
+description: "",
+location: "2B Biostorage 1",
+effects: ["High Health", "High Damage Resistence", "Attacks two-hit every non-infected player and stuns"],
+weaknesses: ["Fire"],
+},
+"BIMF-20": {
+slug: "bimf-20",
+logo: "/viruses/BIMF-20.png",
+slangName: "Crimson Cyst",
+scientificName: "BIMF-20",
+expandedScientificName: "Bio-Ingested Mutagenic Fluid - Year 20",
+description: "A pink liquid virus that infects through ingestion or open wounds. It rapidly converts other liquids into itself. Once infected, it causes veins to burst, leading to bruising and pinkish/purple skin. Blood cells turn pink, the heart rate increases, and blood pools. Pinkish cysts filled with blood form across the body, acting as a secondary circulation system. Popping these cysts weakens the individual and can lead to death.",
+location: "2B Biostorage 1",
+effects: [],
+weaknesses: []
+}
 }
